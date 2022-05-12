a8190e6d67cfc6b6844771b0a24bc1d8253c83d0
lucatescari <luca@ltescari.ch>
2022-05-12 14:30:44 +0200

    project setup

---
+++ lit/prettier.config.js
@@ -0,0 +1,9 @@
+// Prettier Config Options:
+// https://prettier.io/docs/en/options.html
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    arrowParens: 'avoid',
+};
