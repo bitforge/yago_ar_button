73a5da3e45a77e38b08dee023e6fa5cdefa503e4
lucatescari <luca@ltescari.ch>
2022-05-17 10:57:40 +0200

    types

---
+++ lit/dist/interfaces/ar-button-config.d.ts
@@ -0,0 +1,8 @@
+import LanguageObject from './language-object';
+export default interface ArButtonConfig {
+    arButtonText: LanguageObject;
+    buttonTitle: LanguageObject;
+    popupText: LanguageObject;
+    projectColor: string;
+}
+//# sourceMappingURL=ar-button-config.d.ts.map
---
+++ lit/dist/interfaces/language-object.d.ts
@@ -0,0 +1,7 @@
+export default interface LanguageObject {
+    de: string;
+    en: string;
+    fr: string;
+    it: string;
+}
+//# sourceMappingURL=language-object.d.ts.map
