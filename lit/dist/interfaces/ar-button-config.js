d65122bd54d4c9b116e84a1b1cef38918bd855f8
lucatescari <luca@ltescari.ch>
2022-05-17 10:57:52 +0200

    removed dist stuff

--- lit/dist/interfaces/ar-button-config.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=ar-button-config.js.map
--- lit/dist/interfaces/language-object.d.ts
+++
@@ -1,7 +0,0 @@
-export default interface LanguageObject {
-    de: string;
-    en: string;
-    fr: string;
-    it: string;
-}
-//# sourceMappingURL=language-object.d.ts.map
