7e25a4556440f99798325e76b1509f476d864b15
lucatescari <luca@ltescari.ch>
2022-05-17 10:57:52 +0200

    removed dist stuff

--- lit/dist/interfaces/config.d.ts
+++
@@ -1,8 +0,0 @@
-import ArButtonConfig from './ar-button-config';
-export default interface Config {
-    ar_button_config: ArButtonConfig | null;
-    qr_config: any | null;
-    quicklook_link: string | null;
-    site_url: string | null;
-}
-//# sourceMappingURL=config.d.ts.map
