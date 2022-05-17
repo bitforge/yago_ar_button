05c0c547f9f33b05a78d9ad914ce21abf407945d
lucatescari <luca@ltescari.ch>
2022-05-17 10:46:42 +0200

    half a button works

--- lit/dist/ar-button.d.ts
+++ lit/dist/ar-button.d.ts
@@ -1,7 +1,26 @@
 import { LitElement } from 'lit';
 export declare class ArButton extends LitElement {
     static styles: import("lit").CSSResult;
-    name: string;
+    model: string;
+    qrTitle: string;
+    qrText: string;
+    buttonText: string;
+    baseUrl: string;
+    modelLink: string;
+    config: {
+        site_url: null;
+        quicklook_link: null;
+        qr_config: null;
+        ar_button_config: null;
+    };
+    showQrCode: boolean;
+    showButton: boolean;
+    modalHtml: string;
     render(): import("lit-html").TemplateResult<1>;
+    connectedCallback(): Promise<void>;
+    startAr(e: Event): void;
+    closeModalWindow(): void;
+    getConfig(): Promise<any>;
+    get elementId(): string;
 }
 //# sourceMappingURL=ar-button.d.ts.map
