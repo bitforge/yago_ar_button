fbb2560faebee6f31a5c54c3ea7f4f66f8f6584e
lucatescari <luca@ltescari.ch>
2022-05-17 09:27:00 +0200

    added ts support

---
+++ lit/dist/ar-button.d.ts
@@ -0,0 +1,7 @@
+import { LitElement } from 'lit';
+export declare class ArButton extends LitElement {
+    static styles: import("lit").CSSResult;
+    name: string;
+    render(): import("lit-html").TemplateResult<1>;
+}
+//# sourceMappingURL=ar-button.d.ts.map
