b37a64bbc99b9d618d063293bb438f84d2fda74b
lucatescari <luca@ltescari.ch>
2022-05-18 08:59:47 +0200

    exported ar icon into own component

---
+++ lit/src/components/ar-modal.ts
@@ -0,0 +1,11 @@
+import { html, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+@customElement('ar-modal')
+export class ArModal extends LitElement {
+    render() {
+        return html`
+            I am ar modal you poopy butthole. <br>
+        `;
+    }
+}
